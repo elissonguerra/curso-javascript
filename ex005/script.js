a0b3783e5540d94c195e66c890c5cbc1caec89b0
Elisson Guerra <144069676+elissonguerra@users.noreply.github.com>
2024-06-08 14:16:14 -0300

    Estudos até a aula 9

---
+++ ex005/script.js
@@ -0,0 +1,13 @@
+var d = window.document.getElementById('msg');
+d.style.background = 'green';
+
+var a = window.document.getElementsByTagName('h1')[0];
+a.style.background = 'yellow';
+a.style.color = 'black';
+
+var c = window.document.getElementsByName('res')[0];
+c.style.color = 'black';
+c.style.background = 'white';
+
+var e = window.document.getElementsByClassName('use')[0];
+e.style.background = 'red';
